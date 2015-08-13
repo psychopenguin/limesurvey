d32ecfd87c0db40767c6afdcb3eed3eba546ebaf
Fabio Brito <fabio.brito@movile.com>
2015-08-13 16:09:47 -0300

    itial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='limesurvey',
+      version='0.1',
+      description='Lime Survey Remote Control Interface',
+      url='http://github.com/psychopenguin/limesurvey',
+      author='Fabio Brito',
+      author_email='psychopenguin@gmail.com',
+      license='MIT',
+      packages=['limesurvey'],
+      zip_safe=False)
